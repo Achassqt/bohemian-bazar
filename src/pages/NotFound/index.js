62b826c1f58d0b940435932573ceb99d5c01145d
achas <sachsqt@gmail.com>
2023-11-09 16:43:36 +0100

    feat: add style on page 404 and fix some other style

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <p>404</p>;
+  return (
+    <div className="not-found">
+      <h2>Erreur 404</h2>
+      <div className="not-found__content">
+        <p>
+          Oups ^^ <br />
+          La page demandée n'existe plus. <br />
+          Vous pouvez revenir à la page d'accueil en cliquant juste{" "}
+          <Link to="/">ici !</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
